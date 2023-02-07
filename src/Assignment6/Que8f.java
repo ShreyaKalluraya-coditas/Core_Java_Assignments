aa311da01d721d7cc657f166518091aa6675e7a7
ShreyaKalluraya-coditas <shreya.kalluraya@coditas.com>
2023-02-07 16:59:06 +0530

    Assignment6

--- src/Assignment6/Que8f.java
+++ src/Assignment6/Que8f.java
@@ -2,17 +2,21 @@ package Assignment6;
 
 public class Que8f {
     public static void main(String[] args) {
-        for(int i=1;i<=5;i++)
-        {
-            for(int j=1;j<=5;j++)
-            {
-                if(j<=i)
-                    System.out.print("*");
-                else
-                    System.out.print(" ");
+        for(int i=1;i<=6;i++){
+            char x='A';
+            for(int j=1;j<=i;j++){
+            System.out.print(x++);
+
+
+
+
+
+        }
+            x--;
+            for(int k=2;k<=i;k++){
+                System.out.print(--x);
             }
             System.out.println();
         }
     }
-
 }
