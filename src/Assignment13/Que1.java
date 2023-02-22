78e78881eb16f973d47442502a2e100977ee130f
ShreyaKalluraya-coditas <shreya.kalluraya@coditas.com>
2023-02-22 16:56:49 +0530

    Assignment 13

---
+++ src/Assignment13/Que1.java
@@ -0,0 +1,12 @@
+package Assignment13;
+/* Assertions in Java are a way to ensure that certain conditions are met during runtime.
+To use assertions in Java, you can use the assert keyword followed by a boolean expression.
+An assertion is a statement that checks a boolean expression and throws an AssertionError if the expression is false.
+It is often used for debugging and testing purposes.*/
+public class Que1 {
+    public static void main(String[] args){
+        int x = 7;
+        assert x == 20 : "x is 20";
+        System.out.println("The end");
+    }
+}
